1c7a7f8b85c8c78d89c2729c0e7e1af0ebb1293f
ryo2357 <miyawakir@hosokawa-yoko.co.jp>
2022-11-29 12:39:25 +0900

    見通しをよく

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,16 @@
-//! Blinks the LED on a Pico board
-//!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
 #![no_std]
 #![no_main]
 
 use bsp::entry;
+
+// デバッガのクレート。デバッグピンと通信する準備は必要
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
+
+use embedded_hal::digital::v2::{InputPin, OutputPin};
 use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
@@ -54,15 +51,22 @@ fn main() -> ! {
     );
 
     let mut led_pin = pins.led.into_push_pull_output();
+    let btn = pins.gpio14.into_pull_up_input();
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        if btn.is_low().unwrap() {
+            led_pin.set_high().unwrap();
+        } else {
+            led_pin.set_low().unwrap();
+        }
     }
-}
 
-// End of file
+    // loop {
+    //     info!("on!");
+    //     led_pin.set_high().unwrap();
+    //     delay.delay_ms(500);
+    //     info!("off!");
+    //     led_pin.set_low().unwrap();
+    //     delay.delay_ms(500);
+    // }
+}
